00f026f3b35ba1ce86c049d22c455248c8797b3e
GwynethSamanthaBinz <244960031+GwynethSamanthaBinz@users.noreply.github.com>
2026-06-01 16:43:14 +0200

    infos zu android test zugang hinzugefüägt

--- config.js
+++ config.js
@@ -4,6 +4,11 @@ const CONFIG = {
   model: "qwen3.6-35b-a3b",
 };
 
-//lokaler test über 
+//lokaler test über broswer
 //http://localhost:3000
 
+//lokaler test über android studio 
+//10.0.2.2:PORT
+//10.0.2.2:3000
+//zum starten server :
+//node server.js 
